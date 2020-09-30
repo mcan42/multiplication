85260410667ea8e5dcd551d56ca02d3276436c5d
mcan42 <72167483+mcan42@users.noreply.github.com>
2020-09-30 23:46:18 +0300

    Add files via upload

---
+++ carpimislemi.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <conio.h>
+int main()
+{
+	int a,b,sonuc;
+	printf("1.sayiyi girin= ");
+	scanf("%d",&a);
+	printf("2.sayiyi girin= ");
+	scanf("%d",&b);
+	sonuc=a*b;
+	printf("Carpim sonucu= %d ",sonuc);
+	getch();
+	return 0;
+}
